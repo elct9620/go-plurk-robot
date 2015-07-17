93863c7d3dcc7295e4548e8c3b5c2d273dfe6843
蒼時弦也 <elct9620@frost.tw>
2015-07-17 13:31:00 +0800

    Add Responses.ResponseAdd error test

--- plurk/response_test.go
+++ plurk/response_test.go
@@ -10,21 +10,37 @@ import (
 	"text/template"
 )
 
-func buildResponseAddServer(responseFile string) *httptest.Server {
+func buildResponseAddServer(responseFile string, isError bool, errorText string) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "application/json")
+
+		// Should return error or not
+		if isError {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(200)
+		}
+
 		idString := r.PostFormValue("plurk_id")
 		content := r.PostFormValue("content")
 		qualifier := r.PostFormValue("qualifier")
 
 		id, _ := strconv.Atoi(idString)
 
-		resData := &Response{
+		var resData interface{}
+		resData = &Response{
 			PlurkID:    id,
 			RawContent: content,
 			Content:    content,
 			Qualifier:  qualifier,
+		}
+
+		if isError {
+			resData = &struct {
+				ErrorText string
+			}{
+				ErrorText: errorText,
+			}
 		}
 
 		ts, _ := template.ParseFiles(responseFile)
@@ -40,7 +56,7 @@ func Test_ResponseAdd(t *testing.T) {
 	expectedContent := "Hello World"
 	expectedQualifier := "says"
 
-	server := buildResponseAddServer(testPath)
+	server := buildResponseAddServer(testPath, false, "")
 	defer server.Close()
 
 	plurkClient := &PlurkClient{credential: credential, ApiBase: server.URL}
@@ -50,4 +66,20 @@ func Test_ResponseAdd(t *testing.T) {
 	assert.Equal(t, expectedContent, res.RawContent)
 	assert.Equal(t, expectedPlurkID, res.PlurkID)
 	assert.Equal(t, expectedQualifier, res.Qualifier)
+}
+
+func Test_ResponseAddError(t *testing.T) {
+	testPath := "test/error.json"
+
+	expectedError := "Some error"
+
+	server := buildResponseAddServer(testPath, true, expectedError)
+	defer server.Close()
+
+	plurkClient := &PlurkClient{credential: credential, ApiBase: server.URL}
+	responses := plurkClient.GetResponses()
+	res, err := responses.ResponseAdd(999999, "Hello World", "says")
+
+	assert.Nil(t, res)
+	assert.EqualError(t, err, expectedError)
 }
